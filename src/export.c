ed1084eb85f475fbab81f58252ea984e87671192
lnicosia <lnicosia@student.42.fr>
2019-03-08 14:26:44 +0100

    Fdf export done.

--- src/export.c
+++ src/export.c
@@ -6,26 +6,56 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 12:23:40 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/03/08 12:44:45 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/03/08 14:12:25 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <fcntl.h>
+#include <stdio.h>
 
-int		export_fdf(t_fract fract)
+static int	write_fdf(t_fract fract, int fd)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < 1080)
+	{
+		x = 0;
+		while (x < 1920)
+		{
+			if (x != 0)
+				ft_dprintf(fd, " ");
+			ft_dprintf(fd, "%d,0x%X",
+					(fract.window.img.str[x + y * 1920] / 0xFFFFFF) * 100,
+					fract.window.img.str[x + y * 1920]);
+			x += 3;
+		}
+		ft_dprintf(fd, "\n");
+		y += 3;
+	}
+	return (0);
+}
+
+int			export_fdf(t_fract fract)
 {
 	int		fd;
 	char	*file;
 
 	ft_printf("exporting..\n");
 	file = ft_strjoin(fract.name, ".fdf");
-	if ((fd = open(file, O_CREAT)) < 0)
+	if ((fd = open(file, O_CREAT, 00755)) < 0)
 	{
 		ft_printf("Open error\n");
 		return (-1);
 	}
-	ft_dprintf(fd, "test\n");
+	if ((fd = open(file, O_WRONLY)) < 0)
+	{
+		ft_printf("Open error\n");
+		return (-1);
+	}
+	write_fdf(fract, fd);
 	if (close(fd) == -1)
 		return (-1);
 	ft_printf("%s created successfully!\n", file);
