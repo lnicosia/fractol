225073c21b7d51f669ba63b62c60fc1cf95b1256
lnicosia <lnicosia@student.42.fr>
2019-10-07 15:31:11 +0200

    [ADD] New color for newton's.

--- src/color_newton.c
+++ src/color_newton.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 15:30:44 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/10/07 12:18:23 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/10/07 15:30:02 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,19 @@ int	is_tol(t_complex z, t_complex root, double tolerance)
 void	color_red(int x, int y, t_fract *fract)
 {
 	if (fract->color_mode == FLAT)
-		fract->window.img.str[x + 1024 * y] = 0xFF0000 *
-			(255 * fract->iter / fract->iter_max);
+	{
+		if (fract->color_base == WHITE)
+			fract->window.img.str[x + 1024 * y] = 0xFF0000 *
+				(255 * fract->iter / fract->iter_max);
+		else if (fract->color_base == BLACK)
+			fract->window.img.str[x + 1024 * y] = (255 * fract->iter / fract->iter_max) << 16;
+		else if (fract->color_base == RED)
+			fract->window.img.str[x + 1024 * y] = ((0xFF * fract->iter / fract->iter_max)) << 16
+				| ((0x99 * fract->iter / fract->iter_max)) << 8;
+		else
+			fract->window.img.str[x + 1024 * y] = 0xFF9900 *
+				(255 * fract->iter / fract->iter_max);
+	}
 	else if (fract->color_mode == COS)
 		fract->window.img.str[x + 1024 * y] = 0xFF0000 *
 			cos(fract->iter * 0.01);
@@ -35,8 +46,20 @@ void	color_red(int x, int y, t_fract *fract)
 void	color_green(int x, int y, t_fract *fract)
 {
 	if (fract->color_mode == FLAT)
-		fract->window.img.str[x + 1024 * y] = 0xFF00 *
-			(255 * fract->iter / fract->iter_max);
+	{
+		if (fract->color_base == WHITE)
+			fract->window.img.str[x + 1024 * y] = 0xFF00 *
+				(255 * fract->iter / fract->iter_max);
+		else if (fract->color_base == BLACK)
+			fract->window.img.str[x + 1024 * y] = (255 * fract->iter / fract->iter_max) << 8;
+		else if (fract->color_base == RED)
+			fract->window.img.str[x + 1024 * y] = (0x99 * fract->iter / fract->iter_max) << 16
+				| (0xCC * fract->iter / fract->iter_max) << 8
+				| (0xFF * fract->iter / fract->iter_max);
+		else
+			fract->window.img.str[x + 1024 * y] = 0x99CCFF *
+				(255 * fract->iter / fract->iter_max);
+	}
 	else if (fract->color_mode == COS)
 		fract->window.img.str[x + 1024 * y] = 0xFF00 *
 			cos(fract->iter * 0.01);
@@ -48,8 +71,19 @@ void	color_green(int x, int y, t_fract *fract)
 void	color_blue(int x, int y, t_fract *fract)
 {
 	if (fract->color_mode == FLAT)
-		fract->window.img.str[x + 1024 * y] = 0xFF *
-			(255 * fract->iter / fract->iter_max);
+	{
+		if (fract->color_base == WHITE)
+			fract->window.img.str[x + 1024 * y] = 0xFF *
+				(255 * fract->iter / fract->iter_max);
+		else if (fract->color_base == BLACK)
+			fract->window.img.str[x + 1024 * y] = (255 * fract->iter / fract->iter_max);
+		else if (fract->color_base == RED)
+			fract->window.img.str[x + 1024 * y] = (0x33 * fract->iter / fract->iter_max) << 8
+				| (0x66 * fract->iter / fract->iter_max);
+		else
+			fract->window.img.str[x + 1024 * y] = 0xFF *
+				(255 * fract->iter / fract->iter_max);
+	}
 	else if (fract->color_mode == COS)
 		fract->window.img.str[x + 1024 * y] = 0xFF *
 			cos(fract->iter * 0.01);
@@ -61,7 +95,12 @@ void	color_blue(int x, int y, t_fract *fract)
 void	color_newton(int x, int y, t_fract *fract, t_complex z)
 {
 	if (fract->color_mode == FLAT)
-		fract->window.img.str[x + 1024 * y] = 0xFF * z.r + 0x00FF00 * z.i;
+	{
+		if (fract->color_base == WHITE)
+			fract->window.img.str[x + 1024 * y] = 0xFF * z.r + 0x00FF00 * z.i;
+		else
+			fract->window.img.str[x + 1024 * y] = 0xFF * z.r + 0x00FF00 * z.i;
+	}
 	else if (fract->color_mode == COS)
 		fract->window.img.str[x + 1024 * y] = 0xFF0000 * fabs(cos(z.r * 0.05));
 	else if (fract->color_mode == SIN)
